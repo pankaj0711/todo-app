df36e385d0eb02db119811b5b84dfd65d368a856
pankaj0711 <pankajsharma07059@gmail.com>
2024-10-26 20:37:58 +0530

    code added

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,11 +1,24 @@
 
-
+let head=document.querySelector('h1')
 let frm=document.querySelector('form')
 let lst=document.querySelector('ul')
+let head2=document.querySelectorAll('h2')
+console.log(head2)
+
+head.classList.add('first','third')
+frm.children[1].classList.add('secound')
+head2[0].classList.add('secound')
+head2[1].classList.add('secound')
+
+
+head.classList.add()
 
 frm.addEventListener('submit',(event)=>{
     event.preventDefault()
     let li=document.createElement('li')
+    ///style
+    li.classList.add('fourth')
+    
     if(event.target.children[1].value !=""){
         li.innerText=event.target.children[1].value
     lst.append(li)
